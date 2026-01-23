973d043757d5aabbe326b4c448d59f0fcd4da70b
waterbeenn <beenn09@naver.com>
2026-01-23 18:17:15 +0900

    Feat : 급등주 Top100 리스트 출력 기능 구현, 공공데이터 api 연결

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import MainBanner from './../../components/main/mainBanner/MainBanner';
 import Top100List from './../../components/top100/Top100List';
+import indexData from '../../assets/api/indexs/indexData';
 import '../../reset.css';
 import './style.scss';
-import { useState } from 'react';
-import indexData from '../../assets/api/indexs/indexData';
+
 
 const MainPage = () => {
     const [indexs] = useState(indexData);
--- src/pages/top100/index.jsx
+++ src/pages/top100/index.jsx
@@ -1,5 +1,8 @@
+import Top100List from '../../components/top100/Top100List';
+import './style.scss'
+
 const Top100Page = () => {
-    return <div>top100</div>;
+    return <div><Top100List/></div>;
 };
 
 export default Top100Page;
